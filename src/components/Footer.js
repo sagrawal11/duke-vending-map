285ec643f7533d2356dd41a4b0541c834095037c
sagrawal11 <agrawal.sarthak@gmail.com>
2025-08-25 16:32:55 -0400

    footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="container">
-        <p>© {new Date().getFullYear()} Sarthak Agrawal</p>
+        <p>© {new Date().getFullYear()} Made by Duke students for Duke students</p>
       </div>
     </footer>
   );
