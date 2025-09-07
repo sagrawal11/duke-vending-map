4cc1ac5202e53c63708162678dabf1a8e2877827
sagrawal11 <agrawal.sarthak@gmail.com>
2025-09-07 18:16:34 -0400

    header tag updated

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
               />
             </div>
             <p className="hero-description">
-              Duke's Vending Machine Finder
+              Duke's Guide to Vending Machines and Free Food
             </p>
           </div>
         </div>
